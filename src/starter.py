4dbde06efdef876f6a0631527a845cdeb90c0fc3
Shailendra Jain <shailendrajain28@gmail.com>
2024-05-10 22:57:26 +0100

    moved into src folder

--- src/starter.py
+++ src/starter.py
@@ -5,10 +5,7 @@ from pprint import pprint
 brclient = boto3.client('bedrock')
 
 def get_foundation_models():
-    
-
     models  = brclient.list_foundation_models()
-
     pprint(models)
 
 def get_foundation_model_by_id(id):
