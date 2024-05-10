a4b6b3d4a015bdffe263e01746d37e5300f2a779
Shailendra Jain <shailendrajain28@gmail.com>
2024-05-10 22:52:24 +0100

    starter for bedrock model

---
+++ starter.py
@@ -0,0 +1,17 @@
+import sys
+import boto3
+from pprint import pprint
+
+brclient = boto3.client('bedrock')
+
+def get_foundation_models():
+    
+
+    models  = brclient.list_foundation_models()
+
+    pprint(models)
+
+def get_foundation_model_by_id(id):
+    return brclient.get_foundation_model(modelIdentifier=id)
+
+pprint(get_foundation_model_by_id(sys.argv[1]))
